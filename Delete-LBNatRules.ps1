1ec79089e190cfd3566ba9dca900aa6c12a72fc1
Joachim Stapelfeldt <jos@Joachims-MacBook-Pro.local>
2017-08-09 11:13:20 +0200

    Delete Natrules in a LB. First commit.

---
+++ Delete-LBNatRules.ps1
@@ -0,0 +1,41 @@
+<#
+.Synopsis
+   Delete multiple NAT rules on an Azure Loadbalancer. 
+.DESCRIPTION
+   Gives the possibility to delete multiple NAT rules on an Azure Loadbalancer. Select Frontend or Backendport and the script will delete every match in the resource group provided.
+.EXAMPLE
+   Delete-LBNatRules.ps1 -ResourceGroup "NameOfResourceGroup" -BackendPort 3389
+.EXAMPLE
+   Delete-LBNatRules.ps1 -ResourceGroup "NameOfResourceGroup" -FrontendPort 6578
+#>
+
+param (
+    [Parameter(Mandatory=$true)][string]$ResourceGroup,
+    [Parameter()][string]$BackendPort,
+    [Parameter()][string]$FrontendPort
+)
+
+$LoadBalancers = Get-AzureRmLoadBalancer -ResourceGroupName $ResourceGroup
+
+
+
+foreach($LB in $LoadBalancers) {
+    $LoadBalancerNatRules = $LB | Get-AzureRmLoadBalancerInboundNatRuleConfig
+
+    if($FrontendPort) {
+        $LBrulesList = $LoadBalancerNatRules | ? {$_.FrontendPort -eq $FrontendPort}
+    }
+    elseif($BackendPort) {
+        $LBrulesList = $LoadBalancerNatRules | ? {$_.BackendPort -eq $BackendPort}
+    }
+    else {
+        Write-Host "Please provide Backend or Frontend port"
+        return
+    }
+
+    foreach($LBRuleToRemove in $LBrulesList) {
+        Write-Host "Removing rule:" $LBRuleToRemove.Name "source:"$LBRuleToRemove.FrontendPort"target:"$LBRuleToRemove.BackendPort"on loadbalancer:" $LB.Name
+        Remove-AzureRmLoadBalancerInboundNatRuleConfig -Name $LBrulesList.Name -LoadBalancer $LB
+        write-host "Rule removed." -ForegroundColor Cyan
+    }
+}
